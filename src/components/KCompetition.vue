abeff179fb9c95001d508e5b472b208d45ed4ee8
JY29 <344159020@qq.com>
2017-11-16 00:35:38 +0800

    修改竞猜样式

--- src/components/KCompetition.vue
+++ src/components/KCompetition.vue
@@ -8,42 +8,42 @@
       <div class="icon-bar-item icon-bar-more">投注记录</div>
     </div>
     <div class="info-board">
-      <div class="info-board-body">40000</div>
+      <div class="info-board-body">{{gold}}</div>
     </div>
     <div>
-      <div class="vs">
-        <div class="one">
-          <div class="title">蓝方</div>
-          <div class="list">
+      <div class="vs" v-for="game in games">
+        <div class="vs-left">
+          <div class="vs-left-title">蓝方</div>
+          <div class="vs-left-list">
             <ul>
-              <li><img src="../assets/icon.jpeg"/>白金</li>
-              <li><img src="../assets/icon.jpeg"/>白金</li>
-              <li><img src="../assets/icon.jpeg"/>白金</li>
-              <li><img src="../assets/icon.jpeg"/>白金</li>
-              <li><img src="../assets/icon.jpeg"/>白金</li>
+              <li><img class="vs-icon" :src="game.members[0].mb_avatar_url"/>{{game.members[0].mb_nickname}}</li>
+              <li><img class="vs-icon" :src="game.members[1].mb_avatar_url"/>{{game.members[1].mb_nickname}}</li>
+              <li><img class="vs-icon" :src="game.members[2].mb_avatar_url"/>{{game.members[2].mb_nickname}}</li>
+              <li><img class="vs-icon" :src="game.members[3].mb_avatar_url"/>{{game.members[3].mb_nickname}}</li>
+              <li><img class="vs-icon" :src="game.members[4].mb_avatar_url"/>{{game.members[4].mb_nickname}}</li>
             </ul>
           </div>
-          <div class="margin30">1.5倍</div>
-          <div class="result">蓝方胜</div>
+          <div class="vs-rate">1.5倍</div>
+          <div class="vs-win">蓝方胜</div>
         </div>
-        <div class="two">
-          <div class="title">正在进行中</div>
-          <div class="time">08:00</div>
-          <div class="vs_word">V.S</div>
+        <div class="vs-middle">
+          <div class="vs-middle-title">{{getStatus(game.r_status)}}</div>
+          <div class="vs-middle-time">08:00</div>
+          <div class="vs-middle-icon">V.S</div>
         </div>
-        <div class="three">
-          <div class="title">红方</div>
-          <div class="list">
+        <div class="vs-right">
+          <div class="vs-right-title">红方</div>
+          <div class="vs-right-list">
             <ul>
-              <li>白金<img src="../assets/icon.jpeg"/></li>
-              <li>白金<img src="../assets/icon.jpeg"/></li>
-              <li>白金<img src="../assets/icon.jpeg"/></li>
-              <li>白金<img src="../assets/icon.jpeg"/></li>
-              <li>白金<img src="../assets/icon.jpeg"/></li>
+              <li>{{game.members[5].mb_nickname}}<img class="vs-icon" :src="game.members[5].mb_avatar_url"/></li>
+              <li>{{game.members[6].mb_nickname}}<img class="vs-icon" :src="game.members[6].mb_avatar_url"/></li>
+              <li>{{game.members[7].mb_nickname}}<img class="vs-icon" :src="game.members[7].mb_avatar_url"/></li>
+              <li>{{game.members[8].mb_nickname}}<img class="vs-icon" :src="game.members[8].mb_avatar_url"/></li>
+              <li>{{game.members[9].mb_nickname}}<img class="vs-icon" :src="game.members[9].mb_avatar_url"/></li>
             </ul>
           </div>
-          <div class="margin30">1.2倍</div>
-          <div class="result">红方输</div>
+          <div class="vs-rate">1.2倍</div>
+          <div class="vs-win">红方胜</div>
         </div>
       </div>
     </div>
@@ -76,8 +76,8 @@
     font-size: 28px;
     color: #fff;
     background-color: #231927;
-    border-top:2px solid #03587C;
-    border-bottom:2px solid #03587C;
+    border-top: 2px solid #03587C;
+    border-bottom: 2px solid #03587C;
   }
 
   .icon-bar-item {
@@ -94,6 +94,11 @@
     margin-right: 30px;
   }
 
+  .vs-icon {
+    height: 80px;
+    width: 80px;
+  }
+
   .info-board {
     height: 240px;
     width: auto;
@@ -101,8 +106,8 @@
     margin-right: 20px;
     margin-left: 20px;
     margin-top: 100px;
-    font-weight:bold;
-    border-bottom:2px solid #03587C;
+    font-weight: bold;
+    border-bottom: 2px solid #03587C;
   }
 
   .info-board-body {
@@ -116,7 +121,7 @@
     line-height: 40px;
     font-size: 20px;
     position: relative;
-    border:2px solid #1DACD9;
+    border: 2px solid #1DACD9;
     top: -45%;
     left: 55%;
     border-radius: 20px;
@@ -126,63 +131,122 @@
     font-size: 30px;
   }
 
-  .container .vs {
-    display: flex;
-    display: -webkit-flex;
+  .vs {
     width: 100%;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: space-between;
+    color: #ffffff;
+    margin-top: 20px;
+    font-size: 30px;
+    height: 700px;
+    border-bottom: 2px solid #1DACD9;
   }
-  .container .vs .one {
-    margin: 0.3rem 0;
-  }
-  .container .vs .one .title{
-    color: blue;
-    height: 1rem;
-  }
-  .container .vs .one .list ul li{
-    list-style: none;
-    margin-left: -0.48rem;
-    line-height: 0.8rem;
-  }
-  .container .vs .one .list img {
 
-  }
-  .container .vs .one .result {
-    border: 1px solid #ccc;
-    border-radius: 0.08rem;
-    padding: 0.2rem 0.5rem;
-  }
-  .container .vs .two {
-    margin: 0.3rem 0;
-  }
-  .container .vs .two .time{
-    text-align: center;
-  }
-  .container .vs .two .vs_word {
-    text-align: center;
-    font-size: 1.2rem;
-    font-weight: bold;
-  }
-  .container .vs .three {
-    margin: 0.3rem 0;
-  }
-  .container .vs .three .title{
-    color: red;
-    height: 1rem;
-  }
-  .container .vs .three .result {
-    border: 1px solid #ccc;
-    border-radius: 0.08rem;
-    padding: 0.2rem 0.5rem;
-  }
-  .container .vs .three .list ul li{
+  .vs-left {
     list-style: none;
-    margin-left: -0.48rem;
-    line-height: 0.8rem;
+    display: inline;
+    float: left;
+    text-align: left;
+    margin-left: 3%;
+    width: 30%;
   }
-  .container .margin30{
-    margin: 0.3rem 0;
+
+  .vs-middle {
+    width: 34%;
+    display: inline;
+    float: left;
+  }
+
+  .vs-right {
+    width: 30%;
+    list-style: none;
+    display: inline;
+    float: right;
+    text-align: right;
+    margin-right: 3%;
+  }
+
+  .vs-left ul {
+    list-style: none;
+  }
+
+  .vs-right ul {
+    list-style: none;
+  }
+
+  .vs-left ul li {
+    height: 100px;
+    line-height: 100px;
+  }
+
+  .vs-right ul li {
+    height: 100px;
+    line-height: 100px;
+  }
+
+  .vs-left-list {
+    margin-top: 10px;
+  }
+
+  .vs-right-list {
+    margin-top: 10px;
+  }
+
+  .vs-middle-icon {
+    font-size: 160px;
+    margin-top: 100px;
+  }
+
+  .vs-left-title {
+    color: blue;
+  }
+
+  .vs-right-title {
+    color: red;
+  }
+
+  .vs-rate {
+    margin-top: 10px;
+    height: 50px;
+    line-height: 50px;
+    text-align: center;
+  }
+
+  .vs-win {
+    margin-top: 10px;
+    height: 50px;
+    line-height: 50px;
+    text-align: center;
+    border: 1px solid #ffffff;
   }
 </style>
+<script>
+  import Give from './KGive.vue'
+  export default {
+    data () {
+      return {
+        games: [],
+        gold: '',
+      }
+    },
+    components: {
+      Give,
+    },
+    beforeMount() {
+      this.axios.get('/user/gameRoomInfo').then(result => {
+        if (result.data && result.data.code === 200) {
+          this.games = result.data.data;
+        }
+      }).catch(window.alert);
+      this.axios.get('/user/gold').then(result => {
+        if (result.data && result.data.code === 200) {
+          this.gold = result.data.data.mb_gold;
+        }
+      }).catch(window.alert);
+    },
+    methods: {
+      getStatus(status){
+        const STATUS = ['未开始', '进行中', '已结束', '已结算'];
+        return STATUS[status] || '状态异常';
+      }
+    }
+  }
+</script>
